3f834616a5f196c9e85bb1e09edb84d97909c5fc
BQ97 <1797970331@qq.com>
2023-03-22 08:52:18 +0800

    upgrade

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -34,15 +34,15 @@ class ComposerAutoloaderInit55ea5d3908558f51600f3aa8914d742c
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit55ea5d3908558f51600f3aa8914d742c::$files;
-        $requireFile = static function ($fileIdentifier, $file) {
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
             if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
                 $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
 
                 require $file;
             }
-        };
+        }, null, null);
         foreach ($filesToLoad as $fileIdentifier => $file) {
-            ($requireFile)($fileIdentifier, $file);
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
